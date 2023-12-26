8951f81e31147d90a4dfe45ed1e4262460304006
liqin <3029695059@qq.com>
2023-12-26 20:21:00 +0800

    elementPlus自动按需引入配置

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -6,6 +9,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
     vue(),
   ],
   resolve: {
